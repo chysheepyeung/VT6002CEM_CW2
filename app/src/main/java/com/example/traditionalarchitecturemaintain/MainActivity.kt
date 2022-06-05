fb1251ffe7a35a995e2d5bcd0fc33ed1022be916
Alvin Cheung <alvin941028@gmail.com>
2022-06-05 21:55:44 +0800

    -Create Order UI done

--- app/src/main/java/com/example/traditionalarchitecturemaintain/MainActivity.kt
+++ app/src/main/java/com/example/traditionalarchitecturemaintain/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.traditionalarchitecturemaintain
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToCreateOrder(){
+        val intent= Intent(this,CreateOrderActivity::class.java)
+
+        startActivity(intent)
     }
 }
