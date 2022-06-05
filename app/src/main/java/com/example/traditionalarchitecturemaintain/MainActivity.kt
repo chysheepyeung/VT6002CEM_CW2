238c8d52f1e67e43a43b12f2908d1d4c2a043b7e
Alvin Cheung <alvin941028@gmail.com>
2022-06-05 22:06:16 +0800

    -minor change on Create Order UI

--- app/src/main/java/com/example/traditionalarchitecturemaintain/MainActivity.kt
+++ app/src/main/java/com/example/traditionalarchitecturemaintain/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.traditionalarchitecturemaintain
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun goToCreateOrder(){
+    fun goToCreateOrder(view: View){
         val intent= Intent(this,CreateOrderActivity::class.java)
 
         startActivity(intent)
